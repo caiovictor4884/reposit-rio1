cfc0bc8d2494f577df4071f68a828143cb6cfdf3
caiovictor4884 <caiovictor4884@gmail.com>
2022-01-23 17:42:04 -0300

    2

---
+++ site2/444.js
@@ -0,0 +1,63 @@
+
+
+
+function clicar2() {
+var img = document.getElementById('olhos')
+img.src = 'imges/qwe1.png'
+}
+
+function clicar3() {
+var img = document.getElementById('olhos')
+img.src = 'imges/qwe2.png'
+}
+
+function clicar6() {
+var img = document.getElementById('olhos')
+img.src = 'imges/qwe3.png'
+}
+
+function clicar9() {
+var img = document.getElementById('olhos')
+img.src = 'imges/qwe4.png'
+}
+
+function clicar8() {
+    var img = document.getElementById('olhos')
+    img.src = 'imges/qwe5.png'
+    }
+
+function clicar7() {
+    var img = document.getElementById('olhos')
+    img.src = 'imges/qwe6.png'
+    }
+
+function clicar4() {
+var img = document.getElementById('olhos')
+img.src = 'imges/qwe7.png'
+}
+
+function clicar1() {
+    var img = document.getElementById('olhos')
+    img.src = 'imges/qwe8.png'
+    }
+
+function clicar5() {
+var img = document.getElementById('olhos')
+img.src = 'imges/qwe9.png'
+}
+
+function clicarvero() {
+    var img = document.getElementById('olhos')
+    img.src = 'imges/qwe100.png'
+    }
+
+
+
+
+
+
+
+
+
+
+
